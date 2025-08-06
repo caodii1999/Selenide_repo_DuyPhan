2d5bb566e47f605be16bcf6b62c25923305555d6
Cao Duy <110084193+caodii1999@users.noreply.github.com>
2025-08-06 12:17:43 +0700

    Implementing TC08 and up

---
+++ src/test/java/test/TC_08_TestUsersCanClearCart.java
@@ -0,0 +1,65 @@
+package test;
+
+import dataprovider.UserDataProvider;
+import enums.NavItems;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import model.Product;
+import model.User;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.CheckoutPage;
+import pages.HomePage;
+import pages.MyAccountPage;
+import pages.OrderStatusPage;
+import pages.ProductsPage;
+import pages.ShoppingCartPage;
+
+@Slf4j
+@Test(dataProvider = "userData", dataProviderClass = UserDataProvider.class)
+public class TC_08_TestUsersCanClearCart extends TestBase {
+
+  SoftAssert softAssert = new SoftAssert();
+  MyAccountPage myAccountPage = new MyAccountPage();
+  HomePage homePage = new HomePage();
+  ProductsPage productsPage = new ProductsPage();
+  ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+  CheckoutPage checkoutPage = new CheckoutPage();
+  OrderStatusPage orderStatusPage = new OrderStatusPage();
+
+  List<Product> expectedProductsNames;
+  List<Product> actualProductsNames;
+
+  public void precondition(User user) {
+    homePage.clickOnMyAccountButton();
+
+//  2. Login with valid credentials
+    myAccountPage.login(user);
+
+//  3. Go to Shop page
+    myAccountPage.clickOnNavItem(NavItems.SHOP.getItemName());
+
+//  4. Select multiple items and add to cart
+    expectedProductsNames = productsPage.addMultipleProductsToCartAndGetNames(3);
+
+//  5. Go to the cart and verify all selected items
+    productsPage.clickOnMyCartButton();
+
+    actualProductsNames = shoppingCartPage.getAllProductsNames();
+  }
+
+  public void TestUsersCanClearCart(User user) {
+
+    homePage.clickOnMyAccountButton();
+
+    myAccountPage.login(user);
+
+    myAccountPage.clickOnMyCartButton();
+
+    softAssert.assertEquals(actualProductsNames, expectedProductsNames,
+        "Verify items show in table");
+
+
+  }
+
+}
